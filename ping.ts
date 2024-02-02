06f327d256284d1fd29d45eeea81941cfc89c688
Egor Koleda <radioegor146@yandex.ru>
2024-02-02 09:45:29 +0300

    add ping functionality

--- ping.ts
+++ ping.ts
@@ -1,3 +1,13 @@
+import {promise as pingPromise} from "ping";
+
 export async function pingHost(dev: string, host: string, timeout: number): Promise<bigint> {
-    return 0n;
+    const result = await pingPromise.probe(host, {
+        numeric: true,
+        timeout: timeout / 1000,
+        extra: [`-I`, dev]
+    })
+    if (result.time === "unknown") {
+        throw new Error(`Ping error: ${result.output}`);
+    }
+    return BigInt(Math.floor(result.time * 1000 * 1000));
 }
